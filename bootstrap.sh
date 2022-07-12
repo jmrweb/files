921e1c97d83b156ebac3e849979bcf68fde1e644
jmrweb <63976555+jmrweb@users.noreply.github.com>
2022-07-11 22:51:05 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,4 @@ sudo apt install -y ansible
 mkdir /tmp/bootstrap
 wget https://github.com/jmrweb/files/tarball/master -O /tmp/bootstrap.tgz
 tar xzvf /tmp/bootstrap.tgz --strip-components=1 -C /tmp/bootstrap
-ansible-playbook /tmp/bootstrap/regolith_setup.yaml -K
+ansible-playbook /tmp/bootstrap/ansible/regolith_setup.yaml -K

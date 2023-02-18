8dbfba57790f24b6dcf667b535824bb72cad1ccf
jmrweb <63976555+jmrweb@users.noreply.github.com>
2023-02-18 15:14:50 -0500

    Create regolith_bootstrap.sh

---
+++ regolith_bootstrap.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+sudo apt install -y ansible
+mkdir /tmp/bootstrap
+wget https://github.com/jmrweb/files/tarball/master -O /tmp/bootstrap.tgz
+tar xzvf /tmp/bootstrap.tgz --strip-components=1 -C /tmp/bootstrap
+ansible-playbook /tmp/bootstrap/ansible/regolith_setup.yaml -K
